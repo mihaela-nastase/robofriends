7c836d5b89dc9835f151dbace6a91d108fa20925
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2019-04-01 03:58:42 +0300

    Organize files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
 
 class App extends Component {
