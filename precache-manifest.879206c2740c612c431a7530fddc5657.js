9cfdc2e01a207522519a9a3a6a5a3b30d4267976
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2019-06-17 01:04:03 +0300

    Updates

---
+++ precache-manifest.879206c2740c612c431a7530fddc5657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "5ab804bafe9479550080",
+    "url": "/robofriends/static/js/main.5ab804ba.chunk.js"
+  },
+  {
+    "revision": "9825ca79d152a4e5bfc5",
+    "url": "/robofriends/static/js/2.9825ca79.chunk.js"
+  },
+  {
+    "revision": "5ab804bafe9479550080",
+    "url": "/robofriends/static/css/main.a7f4de7e.chunk.css"
+  },
+  {
+    "revision": "9825ca79d152a4e5bfc5",
+    "url": "/robofriends/static/css/2.aa29a52c.chunk.css"
+  },
+  {
+    "revision": "e0cbdb69bf76ab98a6b305fd5a00c0a9",
+    "url": "/robofriends/index.html"
+  }
+];
